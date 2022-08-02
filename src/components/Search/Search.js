3ace2c91e7c52c68fea3783db424bd8eded3c48e
MartaMalovana <marta.malovana@gmail.com>
2022-08-02 22:33:55 +0300

    1

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import Select from 'react-select';
+import axios from "axios";
 import {Form, Label, Input, Button, Title, City} from "./Search.styled";
 import CitiesList from "../CitiesList/CitiesList";
 
+const fetch = axios.create({
+    baseURL: 'https://localhost:3000'
+});
+
+const schedule = async function fetchAPI () {
+    const response = await fetch.get('/lviv');
+    return response ? response.data : console.log('Fetch error');
+};
+
 export default function Search () {
+    const [fromStation, setFromStation] = useState(null);
 
     const cities = [
         {value: "lviv", label: "Lviv"},
@@ -27,17 +38,24 @@ export default function Search () {
 
     const getOption = (e) => {
         console.log(e.value);
-    }
+        setFromStation(e.value);
+    };
+
+    const getSchedule = (e) => {
+        e.preventDefault();
+        console.log(fromStation);
+        schedule();
+    };
 
     return (
         <>
         <Title>Розклад руху потягів</Title>
-        <Form name="form" type="submit">
+        <Form name="form" type="submit" onSubmit={getSchedule}>
             <Label>від станції</Label>
             <Select name="cityFrom" styles={customStyles} options={cities} defaultValue={cities[0]} onChange={e=>getOption(e)}/>
             <Label>до станції </Label>
             <Select name="city" styles={customStyles} options={cities} defaultValue={cities[0]}/>
-            <Button type="button">Знайти</Button>
+            <Button type="submit">Знайти</Button>
         </Form>
         </>
     );
